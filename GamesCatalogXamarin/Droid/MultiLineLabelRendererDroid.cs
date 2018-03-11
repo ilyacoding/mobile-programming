941de1553434a7f483d3f6276fdb8318e23c4f0d
Ilya Kovalenko <ilyacoding@yandex.com>
2018-03-11 03:48:48 +0300

    Add MultiLine Label support for both platforms in Xamarin

---
+++ GamesCatalogXamarin/Droid/MultiLineLabelRendererDroid.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Android.Content;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(GamesCatalogXamarin.MultiLineLabel), typeof(GamesCatalogXamarin.Droid.MultiLineLabelRendererDroid))]
+namespace GamesCatalogXamarin.Droid
+{
+    public class MultiLineLabelRendererDroid : LabelRenderer
+    {
+        public MultiLineLabelRendererDroid(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            var baseLabel = (MultiLineLabel)Element;
+
+            if (baseLabel != null)
+            {
+                Control.SetSingleLine(false);
+                Control.SetLines(baseLabel.Lines);
+            }
+        }
+    }
+}
---
+++ GamesCatalogXamarin/iOS/MultiLineLabelRendererIOS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(GamesCatalogXamarin.MultiLineLabel), typeof(GamesCatalogXamarin.iOS.MultiLineLabelRendererIOS))]
+namespace GamesCatalogXamarin.iOS
+{
+    public class MultiLineLabelRendererIOS : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            var baseLabel = Element as MultiLineLabel;
+
+            if (baseLabel != null && baseLabel.Lines != -1)
+            {
+                Control.Lines = baseLabel.Lines;
+            }
+        }
+    }
+}
